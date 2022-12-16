a5c13998b026069e3a864423adc5407e844386d4
JeffSabol <jeffreysabol345@gmail.com>
2022-12-15 19:15:05 -0500

    Randomize colors only changes 1/4 of the pixels now, added chromatic aberration, but it only works on transparent .png files

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@
 // Add more effects
 // Add error handling for incorrect file types
 // Give file extension selection option
+// Add additional option to let random pixel color to ignore white pixels (maybe different shades of white could screw this up)
+// Make chromatic aberration work with other file types besides transparent PNGs
+//     Let the user select colors for chromatic aberration
 
 import java.awt.event.*;
 import java.io.*;
@@ -15,6 +18,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 
 public class Main {
   public static void main(String[] args) {
@@ -41,6 +45,7 @@ public class Main {
     JMenu effects = new JMenu("Effects");
     JMenuItem invertColors = new JMenuItem("Invert Colors");
     JMenuItem randomColors = new JMenuItem("Randomize Colors");
+    JMenuItem chromaticAberration = new JMenuItem("Chromatic Aberration ");
 
     // Menu bar choices
     menuBar.add(fileMenu);
@@ -51,6 +56,7 @@ public class Main {
     menuBar.add(effects);
     effects.add(invertColors);
     effects.add(randomColors);
+    effects.add(chromaticAberration);
 
     // Open Image and display on screen
     openMenuItem.addActionListener(new ActionListener() { // TODO: Add proper error handling when incorrect file is
@@ -141,7 +147,7 @@ public class Main {
       }
     });
 
-    randomColors.addActionListener(new ActionListener(){
+    randomColors.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         Icon icon = labelImage.getIcon();
         BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
@@ -155,11 +161,12 @@ public class Main {
             int r = (p >> 16) & 0xff;
             int g = (p >> 8) & 0xff;
             int b = p & 0xff;
-            r = (int)(Math.random() * 256);
-            g = (int)(Math.random() * 256);
-            b = (int)(Math.random() * 256);
+            r = (int) (Math.random() * 256);
+            g = (int) (Math.random() * 256);
+            b = (int) (Math.random() * 256);
             p = (a << 24) | (r << 16) | (g << 8) | b;
-            image.setRGB(i, j, p);
+            if ((int) (Math.random() * 4 % 4) == 0)
+              image.setRGB(i, j, p);
           }
         }
         // erase old picture
@@ -174,8 +181,66 @@ public class Main {
       }
     });
 
+    chromaticAberration.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Icon icon = labelImage.getIcon();
+        BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics gfx = image.createGraphics();
+        icon.paintIcon(null, gfx, 0, 0);
+        gfx.dispose();
+
+        BufferedImage newImage = new BufferedImage(image.getWidth() + 15, image.getHeight(),
+            BufferedImage.TYPE_INT_ARGB);
+
+        for (int i = 0; i < image.getWidth(); i++) {
+          for (int j = 0; j < image.getHeight(); j++) {
+            int p = image.getRGB(i, j);
+            int a = (p >> 24) & 0xff;
+            int r = (p >> 16) & 0xff;
+            int g = (p >> 8) & 0xff;
+            int b = p & 0xff;
+
+            if (i < newImage.getWidth() / 2) {
+              b = Math.max(0, Math.min(255, b + 254));
+              p = (a << 24) | (r << 16) | (g << 8) | b;
+              if (i - 15 >= 0 && i < image.getWidth()) {
+                for (int k = 1; k <= 15; k++)
+                  newImage.setRGB(i - k, j, p);
+              }
+            } else {
+              r = Math.max(0, Math.min(255, r + 254));
+              p = (a << 24) | (r << 16) | (g << 8) | b;
+              newImage.setRGB(i + 15, j, p);
+            }
+
+          }
+        }
+
+        // combined the images
+        BufferedImage combinedImage = new BufferedImage(image.getWidth(), image.getHeight(),
+            BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = combinedImage.createGraphics();
+        g2d.drawImage(newImage, 0, 0, null);
+        g2d.drawImage(image, 0, 0, null);
+        g2d.dispose();
+
+        // erase old picture
+        frame.getContentPane().removeAll();
+        frame.repaint();
+
+        // need to convert to ImageIcon now
+        ImageIcon ic = new ImageIcon(combinedImage);
+        labelImage.setIcon(ic);
+        frame.add(labelImage);
+        frame.setVisible(true);
+      }
+    });
+
     frame.setJMenuBar(menuBar);
     frame.setSize(1000, 700);
     frame.setVisible(true);
   }
 }
+
+// Randomize colors only changes 25% of the pixels
+// Added chromatic aberration, but it only works with 
